52ab6a3fad286feda54372ea5709ed4b63ad2a08
Trushita Maurya (CW) <Trushita.Maurya@jda.com>
2020-12-02 22:35:32 +0530

    Added drawer and Filter placeholder

---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Card, CardContent, Typography } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import data from "./mockData";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+//TODO: Need to add duration section
+//TODO: remove autocomplete if drawer added
+
+export default function Filter() {
+  const classes = useStyles();
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardContent>
+            <Typography>Filter PlaceHolder</Typography>
+            {/* <Autocomplete
+              id="combo-box-demo"
+              options={data}
+              getOptionLabel={(option) => option.title}
+              style={{ width: 300 }}
+              renderInput={(params) => (
+                <TextField {...params} label="Combo box" variant="outlined" />
+              )}
+            /> */}
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+}
