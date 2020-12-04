3bb326ca5d7dcc18bf1612368122166eda95d412
Trushita Maurya (CW) <Trushita.Maurya@jda.com>
2020-12-04 22:06:59 +0530

    Added tab to charts

--- src/components/Charts/Line/index.js
+++ src/components/Charts/Line/index.js
@@ -31,14 +31,10 @@ const isIgnored = (data) => {
 
 function MinMaxQuotes(props) {
   const chart = useRef(null);
-  //const cData = [];
   const [chartData, setChartData] = useState([]);
+  
   //Rough testing
-  let visits = 10;
-  // for (let i = 1; i < 366; i++) {
-  //   visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-  //   cData.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
-  // }
+ 
   const cData = [{
     "date": new Date(2018, 0, 1),
     "gcp": 6.5,
@@ -66,10 +62,6 @@ function MinMaxQuotes(props) {
     "azure": 2.4
   }];
   
-  
-
-
-  console.log("cccccc", cData)
   if (chartData.length === 0) {
     setChartData(cData);
   }
@@ -128,89 +120,7 @@ function MinMaxQuotes(props) {
       amchart.dispose();
     };
   }, [chartData]);
-  // const tokens = location.state?.signIn;
-  // useEffect(() => {
-  // 	let amchart = am4core.create("minMaxQuotes", am4charts.XYChart);
-  // 	if (chartData.length === 0) {
-  // 		getminQuotevsmaxQuote(
-  // 			{
-  // 				startDate: props.startDate,
-  // 				EndData: props.endDate,
-  // 			},
-  // 			tokens,
-  // 		).then((response) => {
-  // 			const data = response.data.data.assessment_Quote_max_vs_minData;
-  // 			const fdata = [];
-  // 			let cnt = 0;
-  // 			let maxValue = 0;
-  // 				const counts = [];
-  // 				map(data, (dt) => {
-  // 					cnt++;
-  // 					const quotesNo = dt.uniqueid;
-  // 					maxValue = dt.totalamount > maxValue ? dt.totalamount : maxValue;
-  // 					const target = find(counts, (m) => m.refNo === quotesNo);
-  // 					if (target) {
-  // 						if (dt.totalamount > target.maxAmount && dt.totalamount !== 0) {
-  // 							target.maxAmount = dt.totalamount;
-  // 						}
-  // 						if (dt.totalamount < target.minAmount && dt.totalamount !== 0) {
-  // 							target.minAmount = dt.totalamount;
-  // 						}
-  // 					} else {
-  // 						if (dt.totalamount !== 0) {
-  // 							counts.push({
-  // 								uniqueid: cnt,
-  // 								refNo: dt.uniqueid,
-  // 								maxAmount: dt.totalamount,
-  // 								minAmount: dt.totalamount,
-  // 							});
-  // 						}
-  // 					}
-  // 				});
-  // 			setChartData(counts);
-  // 		});
-  // 	}
-  // 	if (chartData.length) {
-  // 		amchart.data = chartData;
-  // 		var dateAxis = amchart.xAxes.push(new am4charts.DateAxis());
-  // 		dateAxis.title.text = "Ref Numbers";
-  // 		var valueAxis = amchart.yAxes.push(new am4charts.ValueAxis());
-  // 		valueAxis.tooltip.disabled = true;
-  // 		valueAxis.title.text = "Min an Max values";
-
-  // 		var series = amchart.series.push(new am4charts.LineSeries());
-  // 		series.dataFields.dateX = "uniqueid";
-  // 		series.dataFields.refNo = "refNo";
-  // 		series.dataFields.valueY = "maxAmount";
-  // 		series.tooltipText = "Max: {valueY.value}\nRef No : {refNo}";
-  // 		series.sequencedInterpolation = true;
-  // 		series.fill = series.stroke;
-  // 		series.fillOpacity = 0.3;
-  // 		series.defaultState.transitionDuration = 1000;
-  // 		series.tensionX = 0.8;
-
-  // 		var series2 = amchart.series.push(new am4charts.LineSeries());
-  // 		series2.dataFields.dateX = "uniqueid";
-  // 		series2.dataFields.refNo = "refNo";
-  // 		series2.dataFields.valueY = "minAmount";
-  // 		series2.sequencedInterpolation = true;
-  // 		series2.defaultState.transitionDuration = 1500;
-  // 		series2.stroke = amchart.colors.getIndex(6);
-  // 		series2.tooltipText = "minAmount: {valueY.value}\nRef No : {refNo}";
-  // 		series2.fill = series2.stroke;
-  // 		series2.tensionX = 0.8;
-
-  // 		amchart.cursor = new am4charts.XYCursor();
-  // 		amchart.cursor.xAxis = dateAxis;
-  // 		amchart.scrollbarX = new am4core.Scrollbar();
-
-  // 		// amchart.legend = new am4charts.Legend();
-  // 	}
-
-  // 	return () => {
-  // 		amchart.dispose();
-  // 	};
-  // }, [chartData, props.startDate, props.endDate]);
+  
 
   const styleDimensions =
     chartData.length > 0
@@ -218,14 +128,14 @@ function MinMaxQuotes(props) {
       : { width: "0", height: "0px" };
   return (
    
-          <Card>
+          <>
             <div id="minMaxQuotes" style={styleDimensions}></div>
             <Skeleton
               variant="rect"
               width={chartData.length > 0 ? 0 : "100%"}
               height={chartData.length > 0 ? 0 : "500px"}
             ></Skeleton>
-          </Card>
+          </>
        
   );
 }
